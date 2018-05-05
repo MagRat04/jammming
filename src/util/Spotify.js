9c7e545b1b1c008d967c01b7e7d9bbe7122936a5
Chase <chasegatyas@gmail.com>
2018-05-04 22:40:54 -0500

    completed through step 79

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,3 +1,19 @@
 const apiKey = '28adf6a9ddc44165bef6d067ef9ec4dc';
 const apiSecret = '4b19f5c73be1430dbd444346198abc82';
-const Spotify = {}
+
+const userAccessToken = '';
+
+const Spotify = {
+    getAccessToken() {
+        const url = window.location.href;
+        const token = url.match(/access_token=([^&]*)/);
+        const time = url.match(/expires_in=([^&]*)/);
+        if(!this.userAccessToken === '') {
+            return userAccessToken;
+        } else if(!token && time === '') {
+           
+        }
+    }
+}
+
+export default Spotify; 
