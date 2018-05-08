36e04f10a74a7b1729c01ec8d43d78519385f09b
Chase <chasegatyas@gmail.com>
2018-05-08 00:23:28 -0500

    corrected authentication errors, moved variables inside of getAccessToken

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,35 +1,32 @@
 const clientID = '28adf6a9ddc44165bef6d067ef9ec4dc';
 const redirectURI = 'http://localhost:3000/';
 
-
+let accessToken;
+let expiresIn;
 
 const Spotify = {
     getAccessToken() {
-        let url = window.location.href;
-        let userAccessToken = window.location.href.match(/access_token=([^&]*)/);
-        let expiresIn = window.location.href.match(/expires_in=([^&]*)/); 
-
-        if (userAccessToken) {
-            console.log('Inside first if');
-            return userAccessToken;
-        } else if (userAccessToken && expiresIn) {
-            userAccessToken = userAccessToken[1];
-            expiresIn = expiresIn[1];
-            window.setTimeout(() => userAccessToken = '', expiresIn * 1000);
+        const url = window.location.href;
+        const token = url.match(/access_token=([^&]*)/);
+        const time = url.match(/expires_in=([^&]*)/);
+        if (accessToken) {
+            return accessToken;
+        } else if (token && time) {
+            accessToken = token[1];
+            expiresIn = time[1]; 
+            window.setTimeout(() => accessToken = null, expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
-            console.log('just aquired a token' + userAccessToken);
-            return userAccessToken;
+      return accessToken;
         } else {
-            window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
-            console.log('going to get a token: ' + userAccessToken);
+            window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token`;
         }
     },
     search(term) {
         this.getAccessToken();
-        console.log('after search token' + this.userAccessToken);
+        console.log('after search token: ' + accessToken);
         console.log('search term: ' + term)
         return fetch(`https://api.spotify.com/v1/search?type=track&limit=20&q=${term}`, {
-            headers: { Authorization: `Bearer ${this.userAccessToken}` }
+            headers: { Authorization: `Bearer ${accessToken}` }
         }).then(response => {
             if(response.ok) {
                 console.log(response.json);
